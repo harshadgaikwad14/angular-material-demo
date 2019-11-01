0f113338479a3457a5fa6589e57f64d8d3a9bb4d
Harshad Gaikwad <harshad.gaikwad@intellectdesign.com>
2019-11-01 10:40:43 +0530

    code added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 // tslint:disable-next-line:max-line-length
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatCheckboxModule, MatRadioModule } from '@angular/material'; // This module added for Button
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material'; // This module added for Button
 
 
 const MatModules = [
   MatButtonModule,
   MatIconModule,
   MatFormFieldModule,
-  MatCheckboxModule,
-  MatRadioModule
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
 ];
 
 @NgModule({
